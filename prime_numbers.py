1748b250b0812987b1b9406c5d48e88500ad40c8
Mehmet Alici <calientesea@gmail.com>
2024-03-05 22:08:26 +0100

    implement naive solution

---
+++ prime_numbers.py
@@ -0,0 +1,43 @@
+import argparse
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        prog="prime_numbers",
+        description="Find 100 prime numbers after a number"
+    )
+    parser.add_argument("number", help="Number after which to find 100 prime numbers.")
+    args = parser.parse_args()
+    return args
+
+
+def is_prime(number: int) -> bool:
+    if number < 2:
+        return False
+    
+    for i in range(2, number - 1):
+        if number % i == 0:
+            return False
+        
+    return True
+
+
+def find_primes(start_after: int, count: int) -> list:
+    start = start_after + 1
+    primes = []
+    
+    i = start
+    while len(primes) < count:
+        if is_prime(i):
+            primes.append(i)
+        i += 1
+    
+    return primes
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    count = 100
+    start_after = args.number
+    numbers = find_primes(start_after=start_after, count=count)
+    print(numbers)
---
+++ test_prime_numbers.py
@@ -0,0 +1,55 @@
+from prime_numbers import is_prime, find_primes
+
+
+def test_is_prime():
+    # non-prime number
+    val = 10
+    assert is_prime(val) == False
+
+    # prime number
+    val = 17
+    assert is_prime(val) == True
+
+    # corner case
+    val = 2
+    assert is_prime(val) == True
+
+    # big number
+    val = 123123123
+    assert is_prime(val) == False
+
+    print("is_prime: All tests passed")
+
+
+def test_find_primes():
+    # normal test 1
+    start_after = 5
+    count = 3
+    primes = find_primes(start_after=start_after, count=count)
+    assert primes == [7, 11, 13]
+
+    # normal test 2
+    start_after = 100
+    count = 2
+    primes = find_primes(start_after=start_after, count=count)
+    assert primes == [101, 103]
+
+    # corner case 1
+    start_after = -100
+    count = 2
+    primes = find_primes(start_after=start_after, count=count)
+    assert primes == [2, 3]
+
+    # big number 
+    start_after = 12500000
+    count = 100
+    primes = find_primes(start_after=start_after, count=count)
+    print(primes)
+
+
+    print("find_primes: All tests passed")
+
+
+if __name__ == "__main__":
+    test_is_prime()
+    test_find_primes()
